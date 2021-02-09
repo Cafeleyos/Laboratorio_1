4ab2ad832c39769967d764179d5a5b39e407b907
Santiago Arrieta <78420224+Arri-71@users.noreply.github.com>
2021-02-09 10:05:23 -0500

    Add files via upload

---
+++ Robot.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Main idea: the class robot must be able to generate a character (object) that can move and interact with other objects.  
+ * -Actually: it has an image associated.
+ * @author (Camilo Muñoz Muñoz) 
+ * @version (1.0)
+ */
+public class Robot extends Actor  
+{
+ 
+
+    /**
+     * Default constructor for objects of class Robot
+     */
+    public Robot()
+    {
+    }
+
+}
